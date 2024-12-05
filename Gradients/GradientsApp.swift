133fbe6e7c613f4c75da17a65f8d2f42f78f2de7
Shrenik Vora <summers.bodegas.0s@icloud.com>
2024-12-05 15:59:01 -0600

    Initial Commit

---
+++ Gradients/GradientsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientsApp.swift
+//  Gradients
+//
+//  Created by SHRENIK VORA on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct GradientsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
